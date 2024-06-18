d630b7d16d54f7d847e4ec7e9a25c76de7fbcb17
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2024-06-18 14:44:02 +0700

    hightlights section

--- src/components/Highlights.tsx
+++ src/components/Highlights.tsx
@@ -1,5 +1,44 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
+
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen h-full overflow-hidden common-padding bg-zinc"
+    >
+      <div className=" screen-max-width">
+        <div className="items-end justify-between w-full mb-12 md:flex">
+          <h1 id="title" className=" section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5 ">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2 " />
+            </p>
+            <p className="link">
+              Watch the Event
+              <img src={rightImg} alt="watch" className="ml-2 " />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
